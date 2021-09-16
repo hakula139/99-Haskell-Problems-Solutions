1d78e040c54eb632bbe0a6d154a1e2d3201af89f
Hakula Chen <i@hakula.xyz>
2021-09-16 16:51:12 +0800

    feat: solved problem 04

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,12 @@ module Main where
 
 -- import qualified Problems.Problem01 as P01
 -- import qualified Problems.Problem02 as P02
-import qualified Problems.Problem03 as P03
+-- import qualified Problems.Problem03 as P03
+import qualified Problems.Problem04 as P04
 
 main :: IO ()
 main = do
   -- P01.test
   -- P02.test
-  P03.test
+  -- P03.test
+  P04.test
---
+++ src/Problems/Problem04.hs
@@ -0,0 +1,20 @@
+-- Find the number of elements of a list.
+
+module Problems.Problem04
+  ( test,
+    myLength,
+  )
+where
+
+import MyUtils (assert)
+
+test :: IO ()
+test = do
+  assert "Problem 04" (length nums) (myLength nums)
+  where
+    nums :: [Integer]
+    nums = [1 .. 99]
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_ : xs) = 1 + myLength xs
