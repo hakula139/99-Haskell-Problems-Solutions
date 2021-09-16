8528f272d73f2f9c0f73651794b4e47ffd1d5560
Hakula Chen <i@hakula.xyz>
2021-09-16 15:51:38 +0800

    feat: solved problem 02

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import qualified Problems.Problem01 as P01
+-- import qualified Problems.Problem01 as P01
+import qualified Problems.Problem02 as P02
 
 main :: IO ()
 main = do
-  P01.test
+  -- P01.test
+  P02.test
---
+++ src/Problems/Problem02.hs
@@ -0,0 +1,23 @@
+-- Find the last but one element of a list.
+
+module Problems.Problem02
+  ( test,
+    myButLast,
+  )
+where
+
+import MyUtils (assert)
+
+test :: IO ()
+test = do
+  assert "Problem 02" prevLastChar (myButLast chars)
+  where
+    chars :: [Char]
+    chars = ['a' .. succ prevLastChar]
+    prevLastChar = 'y'
+
+myButLast :: [a] -> a
+myButLast [] = error "empty list"
+myButLast [_] = error "list size < 2"
+myButLast [x, _] = x
+myButLast (_ : xs) = myButLast xs
